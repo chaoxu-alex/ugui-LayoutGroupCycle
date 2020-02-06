505daa651bb6f3689633ee9dc62610761f0a5a29
alex <alex@10.10.10.123>
2020-02-06 11:43:59 +0800

    Optimization for unnecessary calculations when scrolling

--- Assets/Scripts/LayoutGroupCycle/GridLayoutGroupCycle.cs
+++ Assets/Scripts/LayoutGroupCycle/GridLayoutGroupCycle.cs
@@ -14,11 +14,15 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
     public uint capacity { get { return m_Capacity; } set { SetProperty(ref m_Capacity, value); } }
     public OnPopulateChild onPopulateChild { get; set; }
 
+    protected int m_actualCellCountX = 0;
+    protected int m_actualCellCountY = 0;
+    protected int m_StartIndex = -1;
+    protected int[] m_ChildIndexMap = null;
+    protected Vector2 m_startOffset = Vector2.zero;
     protected Vector2 m_NormalizedPosition = Vector2.zero;
-    private int m_StartIndex = -1;
-    private int[] m_ChildIndexMap = null;
-    private List<GameObject> m_PendingDeactiveList = null;
-    private List<GameObject> m_PendingPopulateList = null;
+    protected Vector2[] m_CellPositionMap = null;
+    protected List<GameObject> m_PendingDeactiveList = new List<GameObject>();
+    protected List<GameObject> m_PendingPopulateList = new List<GameObject>();
 
     protected override void Awake()
     {
@@ -60,7 +64,7 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
             }
         }
 
-        SetCellsAlongAxisCycle(1, true);
+        UpdateChildrenPositions(true);
     }
 
     // this function makes sure Populate() is called while property capacity doesn't when capacity stay unchanged
@@ -75,7 +79,11 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
     public void Populate()
     {
         m_StartIndex = -1;
-        m_ChildIndexMap = Enumerable.Repeat(-1, rectChildren.Count).ToArray();
+
+        for (var i = 0; i < m_ChildIndexMap.Length; ++i)
+        {
+            m_ChildIndexMap[i] = -1;
+        }
 
         SetDirty();
     }
@@ -102,6 +110,32 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
     }
 
     public override void CalculateLayoutInputHorizontal()
+    {
+        UpdateChildren();
+
+        int minColumns = 0;
+        int preferredColumns = 0;
+        if (m_Constraint == Constraint.FixedColumnCount)
+        {
+            minColumns = preferredColumns = m_ConstraintCount;
+        }
+        else if (m_Constraint == Constraint.FixedRowCount)
+        {
+            minColumns = preferredColumns = Mathf.CeilToInt(capacity / (float)m_ConstraintCount - 0.001f);
+        }
+        else
+        {
+            minColumns = 1;
+            preferredColumns = Mathf.CeilToInt(Mathf.Sqrt(capacity));
+        }
+
+        SetLayoutInputForAxis(
+            padding.horizontal + (cellSize.x + spacing.x) * minColumns - spacing.x,
+            padding.horizontal + (cellSize.x + spacing.x) * preferredColumns - spacing.x,
+            -1, 0);
+    }
+
+    protected void UpdateChildren()
     {
         rectChildren.Clear();
         var toIgnoreList = ListPool<Component>.Get();
@@ -138,27 +172,6 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
         }
 
         m_Tracker.Clear();
-
-        int minColumns = 0;
-        int preferredColumns = 0;
-        if (m_Constraint == Constraint.FixedColumnCount)
-        {
-            minColumns = preferredColumns = m_ConstraintCount;
-        }
-        else if (m_Constraint == Constraint.FixedRowCount)
-        {
-            minColumns = preferredColumns = Mathf.CeilToInt(capacity / (float)m_ConstraintCount - 0.001f);
-        }
-        else
-        {
-            minColumns = 1;
-            preferredColumns = Mathf.CeilToInt(Mathf.Sqrt(capacity));
-        }
-
-        SetLayoutInputForAxis(
-            padding.horizontal + (cellSize.x + spacing.x) * minColumns - spacing.x,
-            padding.horizontal + (cellSize.x + spacing.x) * preferredColumns - spacing.x,
-            -1, 0);
     }
 
     public override void CalculateLayoutInputVertical()
@@ -189,7 +202,7 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
     /// </summary>
     public override void SetLayoutHorizontal()
     {
-        SetCellsAlongAxisCycle(0);
+        UpdateLayoutSize();
     }
 
     /// <summary>
@@ -198,36 +211,28 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
     /// </summary>
     public override void SetLayoutVertical()
     {
-        SetCellsAlongAxisCycle(1);
+        CalculateCellPositions();
+
+        UpdateChildrenPositions();
     }
 
-    private void SetCellsAlongAxisCycle(int axis, bool cycling = false)
+    protected void UpdateLayoutSize()
     {
-        // Normally a Layout Controller should only set horizontal values when invoked for the horizontal axis
-        // and only vertical values when invoked for the vertical axis.
-        // However, in this case we set both the horizontal and vertical position when invoked for the vertical axis.
-        // Since we only set the horizontal position and not the size, it shouldn't affect children's layout,
-        // and thus shouldn't break the rule that all horizontal layout must be calculated before all vertical layout.
-
-        if (axis == 0)
+        // Only set the sizes when invoked for horizontal axis, not the positions.
+        for (int i = 0; i < rectChildren.Count; i++)
         {
-            // Only set the sizes when invoked for horizontal axis, not the positions.
-            for (int i = 0; i < rectChildren.Count; i++)
-            {
-                RectTransform rect = rectChildren[i];
+            RectTransform rect = rectChildren[i];
 
-                m_Tracker.Add(this, rect,
-                    DrivenTransformProperties.Anchors |
-                    DrivenTransformProperties.AnchoredPosition |
-                    DrivenTransformProperties.SizeDelta);
+            m_Tracker.Add(this, rect, DrivenTransformProperties.Anchors | DrivenTransformProperties.AnchoredPosition | DrivenTransformProperties.SizeDelta);
 
-                rect.anchorMin = Vector2.up;
-                rect.anchorMax = Vector2.up;
-                rect.sizeDelta = cellSize;
-            }
-            return;
+            rect.anchorMin = Vector2.up;
+            rect.anchorMax = Vector2.up;
+            rect.sizeDelta = cellSize;
         }
+    }
 
+    protected void CalculateCellPositions()
+    {
         float width = rectTransform.rect.size.x;
         float height = rectTransform.rect.size.y;
 
@@ -238,68 +243,115 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
             cellCountX = m_ConstraintCount;
 
             if (capacity > cellCountX)
-                cellCountY = (int)capacity / cellCountX + (capacity % cellCountX > 0 ? 1 : 0);
+            {
+                cellCountY = ((int)capacity + cellCountX - 1) / cellCountX;
+            }
         }
         else if (m_Constraint == Constraint.FixedRowCount)
         {
             cellCountY = m_ConstraintCount;
 
             if (capacity > cellCountY)
-                cellCountX = (int)capacity / cellCountY + (capacity % cellCountY > 0 ? 1 : 0);
+            {
+                cellCountX = ((int)capacity + cellCountY - 1) / cellCountY;
+            }
         }
         else
         {
             if (cellSize.x + spacing.x <= 0)
+            {
                 cellCountX = int.MaxValue;
+            }
             else
+            {
                 cellCountX = Mathf.Max(1, Mathf.FloorToInt((width - padding.horizontal + spacing.x + 0.001f) / (cellSize.x + spacing.x)));
+            }
 
             if (cellSize.y + spacing.y <= 0)
+            {
                 cellCountY = int.MaxValue;
+            }
             else
+            {
                 cellCountY = Mathf.Max(1, Mathf.FloorToInt((height - padding.vertical + spacing.y + 0.001f) / (cellSize.y + spacing.y)));
+            }
         }
 
         int cornerX = (int)startCorner % 2;
         int cornerY = (int)startCorner / 2;
 
-        int cellsPerMainAxis, actualCellCountX, actualCellCountY;
         if (startAxis == Axis.Horizontal)
         {
-            cellsPerMainAxis = cellCountX;
-            actualCellCountX = cellCountX; //Mathf.Clamp(cellCountX, 1, (int)capacity);
-            actualCellCountY = Mathf.Clamp(cellCountY, 1, Mathf.CeilToInt(capacity / (float)cellsPerMainAxis));
+            m_actualCellCountX = cellCountX;
+            m_actualCellCountY = Mathf.Clamp(cellCountY, 1, Mathf.CeilToInt(capacity / (float)cellCountX));
         }
         else
         {
-            cellsPerMainAxis = cellCountY;
-            actualCellCountY = cellCountY; //Mathf.Clamp(cellCountY, 1, (int)capacity);
-            actualCellCountX = Mathf.Clamp(cellCountX, 1, Mathf.CeilToInt(capacity / (float)cellsPerMainAxis));
+            m_actualCellCountY = cellCountY;
+            m_actualCellCountX = Mathf.Clamp(cellCountX, 1, Mathf.CeilToInt(capacity / (float)cellCountY));
         }
 
-        Vector2 requiredSpace = new Vector2(actualCellCountX * cellSize.x + (actualCellCountX - 1) * spacing.x, actualCellCountY * cellSize.y + (actualCellCountY - 1) * spacing.y);
-        Vector2 startOffset = new Vector2(GetStartOffset(0, requiredSpace.x), GetStartOffset(1, requiredSpace.y));
+        Vector2 requiredSpace = new Vector2(m_actualCellCountX * cellSize.x + (m_actualCellCountX - 1) * spacing.x, m_actualCellCountY * cellSize.y + (m_actualCellCountY - 1) * spacing.y);
+        m_startOffset = new Vector2(GetStartOffset(0, requiredSpace.x), GetStartOffset(1, requiredSpace.y));
+
+        if (m_CellPositionMap == null || m_CellPositionMap.Length != capacity)
+        {
+            m_CellPositionMap = new Vector2[capacity];
+        }
+
+        for (int i = 0; i < capacity; i++)
+        {
+            int positionX;
+            int positionY;
+            if (startAxis == Axis.Horizontal)
+            {
+                positionX = i % m_actualCellCountX;
+                positionY = i / m_actualCellCountX;
+            }
+            else
+            {
+                positionX = i / m_actualCellCountY;
+                positionY = i % m_actualCellCountY;
+            }
+
+            if (cornerX == 1)
+            {
+                positionX = m_actualCellCountX - 1 - positionX;
+            }
+            if (cornerY == 1)
+            {
+                positionY = m_actualCellCountY - 1 - positionY;
+            }
+
+            m_CellPositionMap[i].x = m_startOffset.x + (cellSize[0] + spacing[0]) * positionX;
+            m_CellPositionMap[i].y = m_startOffset.y + (cellSize[1] + spacing[1]) * positionY;
+        }
+    }
+
+    protected void UpdateChildrenPositions(bool cycling = false)
+    {
+        float width = rectTransform.rect.size.x;
+        float height = rectTransform.rect.size.y;
 
         var newStartIndex = 0;
-        if (startAxis == Axis.Vertical)
-        {
-            scrollRect.horizontal = true;
-            scrollRect.vertical = false;
-
-            var currentOffset = Mathf.Max(0, width - scrollRect.viewport.rect.size.x) * m_NormalizedPosition.x;
-            var startCol = Mathf.FloorToInt(Mathf.Max(0, currentOffset - startOffset.x) / (cellSize.x + spacing.x));
-            newStartIndex = startCol * actualCellCountY;
-        }
-        else
+        if (startAxis == Axis.Horizontal)
         {
             scrollRect.horizontal = false;
             scrollRect.vertical = true;
 
             var currentOffset = Mathf.Max(0, height - scrollRect.viewport.rect.size.y) * m_NormalizedPosition.y;
-            var startRow = Mathf.FloorToInt(Mathf.Max(0, currentOffset - startOffset.y) / (cellSize.y + spacing.y));
-            newStartIndex = startRow * actualCellCountX;
+            var startRow = Mathf.FloorToInt(Mathf.Max(0, currentOffset - m_startOffset.y) / (cellSize.y + spacing.y));
+            newStartIndex = startRow * m_actualCellCountX;
         }
+        else
+        {
+            scrollRect.horizontal = true;
+            scrollRect.vertical = false;
 
+            var currentOffset = Mathf.Max(0, width - scrollRect.viewport.rect.size.x) * m_NormalizedPosition.x;
+            var startCol = Mathf.FloorToInt(Mathf.Max(0, currentOffset - m_startOffset.x) / (cellSize.x + spacing.x));
+            newStartIndex = startCol * m_actualCellCountY;
+        }
 
         if (!cycling || newStartIndex != m_StartIndex)
         {
@@ -315,28 +367,11 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
                     {
                         var child = rectChildren[childIndex];
 
-                        int positionX;
-                        int positionY;
-                        if (startAxis == Axis.Horizontal)
+                        if (index < capacity)
                         {
-                            positionX = index % cellsPerMainAxis;
-                            positionY = index / cellsPerMainAxis;
+                            SetChildAlongAxis(child, 0, m_CellPositionMap[index].x, cellSize[0]);
+                            SetChildAlongAxis(child, 1, m_CellPositionMap[index].y, cellSize[1]);
                         }
-                        else
-                        {
-                            positionX = index / cellsPerMainAxis;
-                            positionY = index % cellsPerMainAxis;
-                        }
-                        int col = positionX;
-                        int row = positionY;
-
-                        if (cornerX == 1)
-                            positionX = actualCellCountX - 1 - positionX;
-                        if (cornerY == 1)
-                            positionY = actualCellCountY - 1 - positionY;
-
-                        SetChildAlongAxis(child, 0, startOffset.x + (cellSize[0] + spacing[0]) * positionX, cellSize[0]);
-                        SetChildAlongAxis(child, 1, startOffset.y + (cellSize[1] + spacing[1]) * positionY, cellSize[1]);
 
                         if (child.gameObject.activeSelf != index < capacity)
                         {
@@ -349,11 +384,6 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
                             else
                             {
                                 // pending deactivation of children to late update to avoid error metioned above
-                                if (m_PendingDeactiveList == null)
-                                {
-                                    m_PendingDeactiveList = ListPool<GameObject>.Get();
-                                }
-
                                 m_PendingDeactiveList.Add(child.gameObject);
                             }
                         }
@@ -362,13 +392,7 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
                         {
                             m_ChildIndexMap[childIndex] = index;
 
-                            if (m_PendingPopulateList == null)
-                            {
-                                m_PendingPopulateList = ListPool<GameObject>.Get();
-                            }
-
                             m_PendingPopulateList.Add(child.gameObject);
-
                         }
                     }
                 }
@@ -378,31 +402,25 @@ public class GridLayoutGroupCycle : GridLayoutGroup, ILayoutGroupCycle
 
     void LateUpdate()
     {
-        if (m_PendingDeactiveList != null)
+        if (m_PendingDeactiveList.Count > 0)
         {
-            foreach (var go in m_PendingDeactiveList)
-            {
-                go.SetActive(false);
-            }
+            m_PendingDeactiveList.ForEach(go => go.SetActive(false));
 
-            ListPool<GameObject>.Release(m_PendingDeactiveList);
-
-            m_PendingDeactiveList = null;
+            m_PendingDeactiveList.Clear();
         }
-        if (m_PendingPopulateList != null)
+
+        if (m_PendingPopulateList.Count > 0)
         {
-            foreach (var go in m_PendingPopulateList)
+            m_PendingPopulateList.ForEach(go =>
             {
                 var index = m_ChildIndexMap[go.transform.GetSiblingIndex()];
 
                 onPopulateChild?.Invoke(go, index);
 
                 Debug.Log($"GridLayoutGroupCycle.PopulateChild({index})");
-            }
+            });
 
-            ListPool<GameObject>.Release(m_PendingPopulateList);
-
-            m_PendingPopulateList = null;
+            m_PendingPopulateList.Clear();
         }
     }
 }
